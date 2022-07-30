9cbe8556a8dc606f1a2dcde8d91b7cb37b396713
Justin Greco <justingreco@imac.lan>
2022-07-30 14:15:42 -0400

    replaced deprecated watchUtils with reactiveUtils

--- src/components/WebMap/utils/labeling.ts
+++ src/components/WebMap/utils/labeling.ts
@@ -2,7 +2,7 @@ import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
 import Graphic from '@arcgis/core/Graphic';
 import * as geometryEngine from '@arcgis/core/geometry/geometryEngine';
 import Collection from '@arcgis/core/core/Collection';
-import * as watchUtils from '@arcgis/core/core/watchUtils';
+import * as reactiveUtils from '@arcgis/core/core/reactiveUtils';
 import * as arcade from "@arcgis/core/arcade";
 export const getAllPolyLayers = (view: __esri.MapView): Collection<__esri.FeatureLayer> => {
   return view.map.allLayers.filter((layer) => {
@@ -120,7 +120,10 @@ export const handlePolygonLabels = (view: __esri.MapView): void => {
   });
 
   view.map.add(labels);
-  watchUtils.whenTrue(view, 'stationary', () => {
-    updateLabels(labels, view);
-  });
+
+  reactiveUtils.when(
+    () => view.stationary,
+    () => {
+      updateLabels(labels, view);
+    });
 };
