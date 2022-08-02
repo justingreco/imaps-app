a5c28493e505bff0d2f52b9610dd77a71e805fa3
Greco <justin.greco@raleighnc.gov>
2022-08-02 09:37:10 -0400

    changed units to imperial, css customization

--- src/components/Tools/Measure/utils/measure.ts
+++ src/components/Tools/Measure/utils/measure.ts
@@ -4,6 +4,8 @@ export function initializeMeasure(ref: HTMLDivElement, view: MapView) {
   const measurement = new Measurement({
     view: view,
     container: ref,
+    areaUnit: 'imperial',
+    linearUnit: 'imperial'
   });
 
   return measurement;
